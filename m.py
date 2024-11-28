79aff53c73c85af7a400ee0fb9af0cad93facc51
Moganbabu-123 <132484237+goldheartguy@users.noreply.github.com>
2024-11-28 12:57:28 +0530

    .

--- m.py
+++
@@ -1,8 +0,0 @@
-list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
-list2 = ["Piatti","The Grjhbhbhbbbbbbbbbnnhbjjhn khjb nhjmb nubhjnnjjjjjjjjjjjjill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
-indext=[]
-for i in range(len(list1)):
-    for j in range(len(list2)):
-        if list1[i]==list2[j]:
-            indext.append(i+j)
-print(min(indext))          
